a12b7be71cea2698f71960c42b948858011e9818
Lillian <lilynoordhoek@gmail.com>
2023-07-21 17:42:11 -0500

    let and const exercises

---
+++ ES2015/Let and Const/letconst.js
@@ -0,0 +1,8 @@
+const PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/**
+ * 1) Let cannot be redeclared. Both vat and let can be reassinged however. Var does not have block scope
+ * 2) Const cannot be reassinged or redeclared.
+ * 3) Let can be reassigned. Const cannot.
+ */
